4eec9eb2ca9e6ec7a3c5904519a85b0ba118b8df
Andres3 <a.manrique@uniandes.edu.co>
2019-11-18 13:44:25 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es.json";
+import localeEnMessages from "./locales/en.json";
 
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+let language= navigator.language||navigator.userLanguage;
+let message=language==="en"?localeEnMessages:localeEsMessages;
+
+ReactDOM.render(
+	<IntlProvider locale={language} messages= {message}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
